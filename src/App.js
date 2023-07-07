6439c82ed70ea5ec046c5a4e20ffa1d628a88415
prakashuniyal01 <uniyalprakash7@gmail.com>
2023-07-07 21:12:09 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import UserProvider from "./context/userContext";
+import Cart from "./pages/cart/Cart";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Navbar />,
+		children: [
+			{
+				index: true,
+				element: <Home />,
+			},
+			{
+				path: "signin",
+				element: <Login />,
+			},
+			{
+				path: "signup",
+				element: <Register />,
+			},
+			{
+				path: "cart",
+				element: <Cart />,
+			},
+		],
+	},
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<Provider store={store}>
+				<UserProvider>
+					<RouterProvider router={router} />
+				</UserProvider>
+			</Provider>
+		</>
+	);
 }
 
 export default App;
